09f98c82ecc82231c6126c452c2a910f807405e6
Jaehoon Kim <jaehoonkim.dev@gmail.com>
2019-11-13 19:49:44 +0900

    Create_ReminderApp_project
    Create_ReminderApp_project
    
    It has a first app structure for flutter app

---
+++ android/app/src/main/kotlin/com/jaehoon/flutter_app_prt/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jaehoon.flutter_app_prt
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
